19245011174a33ecad083e8898c011d93a4951a1
Ashok Bohara <118334723+coolboygg@users.noreply.github.com>
2023-07-02 08:59:21 +0545

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,34 @@
+<?php 
+require('include/essentials.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panal -Dashboard</title>
+    <?php require('include/links.php'); ?>
+</head>
+
+<body classs="bg-light">
+
+    <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between">
+        <h3 class="mb-0">ADMIN PANAL</h3>
+        <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
+    </div>
+
+
+
+
+
+
+
+
+    <?php require('include/scripts.php'); ?>
+</body>
+
+</html>
